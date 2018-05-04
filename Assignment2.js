be8e21b7cdde28c6ef6ee2de977fd75948dbbe97
TSN Student <student@ulem.org>
2018-05-04 15:54:36 -0400

    assignment

---
+++ Assignment2.js
@@ -0,0 +1,228 @@
+function printint(n){
+	var i = 1;
+	while(i <= n){
+		console.log(i);
+    // adding 1 to i
+		i++;
+	}
+}
+
+// printInt(10);
+
+function printintRev(n){
+while(i >= n){
+//int i in console
+	console.log(i);
+//ubtracting 1 to i
+	i--;
+	}
+}
+// function printintRev(1000function checkInput(x)){
+// 	if (typeof x== 'number') {
+// 		return "number";
+// 	} else if(typeof x =='string') {
+//            return "string"
+//     } else if(typeof x== 'boolean') {	
+// 	 	    return "boolean" 
+//    } else {
+//     	return -1
+//    }
+// }
+
+ // console.log(checkInput("hello"))   
+
+	// var answer = 10;
+	// for(var i = 1; i <= num; i++){
+ //        answer=answer = i;
+	// }
+	// return answer;
+	   
+
+ //    console.log(simpleAdding(10))
+
+
+
+function simpleEvenAdding(num){
+	var answer = 0;
+	for(var i= 2; i <= num; i+=2){
+		answer=answer =i;
+	}
+}
+//console.log(EvenAdding(6));
+
+ //nsole.log(EvenAdding(12));
+
+
+//nsole.log(simpleAdding )
+ //function lettercapitalize(str){
+ 	//var myArray = str.split(" ");
+ //	var myString = ""
+ //	for(var i=0;i<myArray.length;i++){
+// 		var currentWord = myArray[i][0].toUpperCase() + myArray[i].slice(1);
+// 		myString += " " + currentWord;
+// 	}
+// 	return myString.slice(1);
+//}
+ // var str ="Hello World";
+ // var res = str.toUpperCase("you cannot find the answer online ")
+
+//console.log(lettercapitalize("you are welcome"));
+
+//function lettercapitalize(str){
+  //  var myArray = str.split(" ");
+  //  var mySrtring =""
+ //   for(var i=0;i<myArray.length;i++){
+  //      var currentWord = myArray[i][0].toUpperCase() + myArray[i].slice(1);
+  //      myString += " " + currentWord;
+   // }
+  //  return myString.slice(1);
+ //}
+
+//var array = ["John", "Helen", "Mark" ];
+
+//console.log(array.indexOf('Helen'))
+//var Dean = 'Dean';
+//var myfirstName = 'Dean';
+
+//console.log(myfirstName);
+
+
+var array = ['John', 'Helen', 'Fancis', ];
+
+console.log(array.splice('2'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+//  funtion.simpleReverse(str){
+//  	return str;
+//  }
+//  reverseString("hello")
+ 
+// function simpleReverse(str){
+// 	console.log("hello");
+
+// 	document.write(test)
+
+
+
+
+
+
+
+
+
+
+//  funtion.findDiff(arr)
+
+// function simpleadding(n){
+// loop from 1 to n
+// 	for(let i = 1; i <=n; i++){
+// 		result += i; 
+		
+// 	}
+// 	return result;
+// }
+
+// console.log(SimpleAdding(7))
+
+
+
+// function
+
+
+// function simpleOddadding(n){
+// 	var result = 0;
+// 	for(let i = 1;i <= n;i==){
+     
+//          if(i % 2== 1) {
+//          	result +== i;
+
+//          }
+
+// 	}
+// } 
+
+
+
+// console.log(simpleOddAdding(4))
+
+
+
+
+
+// function letterCapitalize(str){
+// spliit;the str into array of words
+// //  var strArray = str.split("")
+
+// 	return;str array
+// //
+// //
+
+
+
+
+// function
+
+
+
+
+
+
+
+
+
+//  return	parseInt(num / 60)
+// }
+
+
+
+
+
+// function findStr(str,Long){
+// //the number of letters in str
+//   let len = str.length;
+//   var accumilator = 0;
+//   for(let i = 0; long.length; i++){
+//     console.log	(long.slice(i,i+len));
+//  }
+// return accumilator;
+
+// }
+// //if this len = 2
+// // if our long is "hello"
+// //longarr =["he", "el",]
+
+
+// function selfDividingNumbers(left,right){
+// 	for (var i = left; i<=right ; i++) {
+// 		//check  if it is the selfDividingNumber
+// 		if(isSelfDividing(i)){
+
+// 		} 
+
+		 
+// 	}
+    
+//     return myArr;
+
+//     // is givrn number self dividing number?
+//     //return true if the given number is self dividing
+//     //return false if the given number is not self dividing
+//     function isSelfDiving(num){
+
+//     }   
+
+
